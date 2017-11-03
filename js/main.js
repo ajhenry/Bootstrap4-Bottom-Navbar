21c907ebfd6a8757e8498fb4904178757def2f8c
Andrew Henry <ajh145@pitt.edu>
2017-11-03 14:28:55 -0400

    Added more example pages

---
+++ js/main.js
@@ -0,0 +1,45 @@
+//Global variable for starting page
+var currentPageId = "page-home";
+var currentSelectorId = "home";
+
+//Function for getting the button ids
+function getButtons(){
+    //List of button ids
+    var list = ["home", "feed", "create", "account"];
+    return list;
+}
+
+//Make sure the window is loaded before we add listeners
+window.onload = function(){
+    var pageIdList = getButtons();
+    //Add an event listener to each button
+    pageIdList.forEach(function(page){
+        document.getElementById(page).addEventListener("click", changePage, false);
+    });
+}
+
+function changePage(){
+    var currentSelector = document.getElementById(currentSelectorId);
+    var currentPage = document.getElementById(currentPageId);
+    var pageId = "page-"+this.id;
+    var page = document.getElementById(pageId);
+    var pageSelector = document.getElementById(this.id);
+    
+    if(page.classList.contains("active")){
+        return;
+    }
+
+    currentSelector.classList.remove("button-active");
+    currentSelector.classList.add("button-inactive");
+    currentPage.classList.remove("active");
+    currentPage.classList.add("inactive");
+
+    pageSelector.classList.remove("button-inactive");
+    pageSelector.classList.add("button-active");
+
+    page.classList.remove("inactive");
+    page.classList.add("active");
+
+    currentSelectorId = this.id;
+    currentPageId = pageId;
+}
